d3802bfabc4fe6bc3b07c01ddcb6b79fb15c3545
yubi-manoj-deshpande <manoj.deshpande@go-yubi.com>
2024-11-10 11:14:29 +0530

    Padding fix

--- src/main/java/org/example/Decryptor.java
+++ src/main/java/org/example/Decryptor.java
@@ -1,13 +1,19 @@
 package org.example;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.OAEPParameterSpec;
+import javax.crypto.spec.PSource;
 import java.security.PrivateKey;
+import java.security.spec.MGF1ParameterSpec;
 import java.util.Base64;
 
 public class Decryptor {
     public static String decrypt(String encryptedMessage, PrivateKey privateKey) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWITHSHA-256ANDMGF1PADDING");
+
+        OAEPParameterSpec oaepParameterSpec = new OAEPParameterSpec("SHA-256", "MGF1",
+                MGF1ParameterSpec.SHA256, PSource.PSpecified.DEFAULT);
+        cipher.init(Cipher.DECRYPT_MODE, privateKey, oaepParameterSpec);
 
         byte[] encryptedBytes = Base64.getDecoder().decode(encryptedMessage);
         byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
